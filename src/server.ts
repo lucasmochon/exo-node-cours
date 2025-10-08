7e4637d9d05f846cb396877e23129cfd348ae65a
Valentin Hardy <valentinhardy56@gmail.com>
2025-10-08 11:20:48 +0200

    mongoose server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import tournamentRoutes from "./tournament/routes/tournamentRoutes";
 
 const app = express();
@@ -7,6 +8,13 @@ const port = 3000;
 app.use(express.json());
 app.use("/", tournamentRoutes);
 
-app.listen(port, () => {
-    console.log(`Serveur listening at http://localhost:${port}`);
-});
+mongoose.connect('mongodb+srv://valentinhardy56_db_user:<db_password>@cluster0.jizx1b2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log('mongodb est connecté')
+        app.listen(port, () => {
+        console.log(`Serveur listening at http://localhost:${port}`);
+    }) 
+    })
+    .catch(() => {
+        console.log('mongo est pas co !')
+    }) 
