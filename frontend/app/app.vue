778599441ca58e98d852fdaad52f2f5eefb0f986
gorbayotte <mochonlucas@gmail.com>
2025-10-10 10:20:50 +0200

    default front app

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+      <NuxtLayout> 
+          <NuxtPage/>
+      </NuxtLayout>
   </div>
 </template>
+
+<script setup></script>
+
+<script scoped></script>
